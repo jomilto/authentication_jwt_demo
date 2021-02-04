c07fbecc1f868bb27595922ae8048cc2da0d229d
JLob Lap <joselobatof@gmail.com>
2021-02-04 14:20:11 -0600

    implementación de JWT

---
+++ jwt-utilities/index.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const [,, option, secret, nameOrToken ] = process.argv;
+
+if(!option || !secret || !nameOrToken){
+    return console.error('Missing arguments');
+}
+
+function signToken(payload, secret) {
+    return jwt.sign(payload,secret);
+}
+
+function verifyToken(token, secret) {
+    return jwt.verify(token,secret);
+}
+
+if(option == 'sign') {
+    console.log('Signing Token...');
+    console.log(signToken({ sub:nameOrToken },secret));
+}else if (option == 'verify'){
+    console.log('Verifing Token...');
+    console.log(verifyToken(nameOrToken,secret));
+}else{
+    console.log('Option needs to be "sign" or "verify".');
+}
